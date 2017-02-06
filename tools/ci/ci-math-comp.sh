f0958bbe18211d7bd45d37ed490e079041fd5cb6
Emilio Jesus Gallego Arias <e+git@x80.org>
2017-02-07 00:39:30 +0100

    [travis] [External CI] [math-comp] workaround make -j bug

--- tools/ci/ci-math-comp.sh
+++ tools/ci/ci-math-comp.sh
@@ -4,8 +4,12 @@
 ci_dir="$(dirname "$0")"
 source ${ci_dir}/ci-common.sh
 
-git clone --depth 3 https://github.com/math-comp/math-comp.git
+git clone --depth 1 https://github.com/math-comp/math-comp.git
 
-# odd_order takes too much time for travis.
-( cd math-comp/mathcomp && sed -i.bak '/odd_order/d' Make && make -j ${NJOBS} )
+# odd_order takes too much time for Travis
+( cd math-comp/mathcomp                              && \
+     sed -i.bak '/PFsection/d'                  Make && \
+     sed -i.bak '/stripped_odd_order_theorem/d' Make && \
+     make Makefile.coq && make -f Makefile.coq -j ${NJOBS} all )
+
 
