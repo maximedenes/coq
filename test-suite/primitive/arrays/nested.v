d58319b8c4ad06f12f9937472ecd08b1e9217530
Maxime Dénès <maxime.denes@inria.fr>
2020-03-01 16:19:46 +0100

    More tests for nested inductives with `array`

--- test-suite/primitive/arrays/nested.v
+++ test-suite/primitive/arrays/nested.v
@@ -2,6 +2,8 @@ Require Import Int63 PArray.
 
 Open Scope array_scope.
 
+Module OneLevel.
+
 Inductive foo : Set :=
   C : array foo -> foo.
 
@@ -19,3 +21,27 @@ Fixpoint f3 (x : foo) {struct x} : False :=
   match x with
   | C t => f3 (PArray.default t)
   end.
+
+End OneLevel.
+
+Module TwoLevels.
+
+Inductive foo : Set :=
+  C : array (array foo) -> foo.
+
+Fixpoint f1 (x : foo) {struct x} : False :=
+  match x with
+  | C t => f1 (t.[0].[0])
+  end.
+
+Fixpoint f2 (x : foo) {struct x} : False :=
+  f2 match x with
+  | C t => t.[0].[0]
+  end.
+
+Fixpoint f3 (x : foo) {struct x} : False :=
+  match x with
+  | C t => f3 (PArray.default (PArray.default t))
+  end.
+
+End TwoLevels.
